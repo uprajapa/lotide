3935604776e99c0152ff846fb535c84a0aa2fbbe
Urvish Prajapati <urvishprajapati412@gmail.com>
2023-10-17 12:37:09 -0400

    assertEqual and head added

---
+++ assertEqual.js
@@ -0,0 +1,10 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
