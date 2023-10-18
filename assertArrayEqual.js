8beb0257c2b1bf88aa82f0534b0c77c698be2a88
Urvish Prajapati <urvishprajapati412@gmail.com>
2023-10-17 22:47:40 -0400

    asserArrayEqual added

---
+++ assertArrayEqual.js
@@ -0,0 +1,21 @@
+let _ = require('lodash');
+
+const assertArraysEqual = function(actual, expected, bool) {
+  if (eqArrays(actual, expected) === bool) {
+    return `✅ Assertion Passed: ${actual} === ${expected}`;
+  } else {
+    return `🛑 Assertion Failed: ${actual} !== ${expected}`;
+  }
+};
+
+const eqArrays = function(actual, expected) {
+  for (const char in actual) {
+    if (actual[char] !== expected[char]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+console.log(assertArraysEqual([1, 2, 3], [1, 2, 3], true));
+console.log(assertArraysEqual(["1", "2", "3"], ["1", "2", 3], true));
