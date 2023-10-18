bd928971b9c566621d5c5a276572da08cf530895
Urvish Prajapati <urvishprajapati412@gmail.com>
2023-10-17 22:34:40 -0400

    eqArrays

---
+++ eqArrays.js
@@ -0,0 +1,21 @@
+let _ = require('lodash');
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function(actual, expected) {
+  for (const char in actual) {
+    if (actual[char] !== expected[char]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]) , false);
