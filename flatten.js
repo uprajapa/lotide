48e0e5ade041ad46aa38e74df618dc195626294e
Urvish Prajapati <urvishprajapati412@gmail.com>
2023-10-18 14:23:34 -0400

    flatten added

---
+++ flatten.js
@@ -0,0 +1,35 @@
+let _ = require('lodash');
+
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    return `✅ Assertion Passed: ${actual} === ${expected}`;
+  } else {
+    return `🛑 Assertion Failed: ${actual} !== ${expected}`;
+  }
+};
+
+let newArray = [];
+const flatten = function(actual) {
+  for (let char of actual) {
+    if (Array.isArray(char)) {
+      if (char.length > 0) {
+        flatten(char);
+      }
+    } else {
+      newArray.push(char);
+    }
+  }
+  return newArray;
+};
+
+const eqArrays = function(actual, expected) {
+  for (const char in actual) {
+    if (actual[char] !== expected[char]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+console.log(assertArraysEqual(flatten([1, 2, [3, 4], 5, [6]]), [1, 2, 3, 4, 5, 6]));
+// console.log(assertArraysEqual(["1", "2", "3"], ["1", "2", 3], true));
