1c257cb476042d9462320e5aacea608ca0e0d143
Urvish Prajapati <urvishprajapati412@gmail.com>
2023-10-23 16:53:48 -0400

    eqArray updated to loops of arrays

--- eqArrays.js
+++ eqArrays.js
@@ -1,5 +1,3 @@
-let _ = require('lodash');
-
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
@@ -10,12 +8,25 @@ const assertEqual = function(actual, expected) {
 
 const eqArrays = function(actual, expected) {
   for (const char in actual) {
-    if (actual[char] !== expected[char]) {
+    console.log(`Inside for loop: ${actual[char]}, ${expected[char]}`);
+    if (Array.isArray(actual[char]) && Array.isArray(expected[char])) {
+      console.log(`Inside if it is Array loop: ${actual[char]}, ${expected[char]}`);
+      if (actual[char].length !== expected[char].length) {
+        return false;
+      } else if (!eqArrays(actual[char], expected[char])) {
+        return false;
+      }
+    } else if (actual[char] !== expected[char]) {
       return false;
     }
   }
   return true;
 };
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]) , false);
+/* assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]) , false); */
+
+// assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4]]), true); // => true
+
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]), false); // => false
+// assertEqual(eqArrays([[2, 3], [4]], [[2, 3], 4]), false); // => false
