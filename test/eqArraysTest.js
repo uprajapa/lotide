a7650079b3a5daefe02452a66ad7594dbcd03052
Urvish Prajapati <urvishprajapati412@gmail.com>
2023-10-24 01:53:37 -0400

    assertArraysEqualTests created

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -1,13 +1,9 @@
 const assertEqual = require('../assertEqual');
 const eqArrays = require('../eqArrays');
-const assertArraysEqual = require('../assertArraysEqual');
 
-/* assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]) , false);
 
 assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4]]), true);
 assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]), false);
-assertEqual(eqArrays([[2, 3], [4]], [[2, 3], 4]), false); */
-
-assertArraysEqual([1, 2, 3], [1, 2, 3], true);
-assertArraysEqual(["1", "2", "3"], ["1", "2", 3], true);
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], 4]), false);
