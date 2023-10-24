cdc1702af12f0b78a4713374fa83be9620d79412
Urvish Prajapati <urvishprajapati412@gmail.com>
2023-10-24 01:27:28 -0400

    tail updated

--- tail.js
+++ tail.js
@@ -1,18 +1,7 @@
 let _ = require('lodash');
 
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const tail = function(tempArray) {
   return _.tail(tempArray);
 };
 
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2);
-assertEqual(result[0], "Lighthouse");
-assertEqual(result[1], "Labs");
+module.exports = tail;
--- test/assertEqualTest.js
+++ test/assertEqualTest.js
@@ -1,7 +1,17 @@
 const assertEqual = require('../assertEqual');
 const head = require('../head');
+const tail = require('../tail');
 
+// assertEqual tests
 assertEqual("Lighthouse Labs", "Bootcamp");
 assertEqual(1, 1);
+
+// head tests
 assertEqual(head([5,6,7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+
+// tail tests
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+assertEqual(result.length, 2);
+assertEqual(result[0], "Lighthouse");
+assertEqual(result[1], "Labs");
