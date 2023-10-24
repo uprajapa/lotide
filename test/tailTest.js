af73c9e3d723e4e2d098be5fb8c9143fb8c34361
Urvish Prajapati <urvishprajapati412@gmail.com>
2023-10-24 01:30:41 -0400

    tail test added

---
+++ test/tailTest.js
@@ -0,0 +1,7 @@
+const assertEqual = require('../assertEqual');
+const tail = require('../tail');
+
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+assertEqual(result.length, 2);
+assertEqual(result[0], "Lighthouse");
+assertEqual(result[1], "Labs");
